49b20b61328a557cc7994e0c18e6a4228df756c8
Jed <vjeudy@protonmail.com>
2023-12-23 22:43:53 +0100

    day 3 a

--- src/day3a.rs
+++ src/day3a.rs
@@ -1,4 +1,4 @@
-use std::{fs, str::Chars};
+use std::fs;
 
 #[derive(Debug)]
 struct Number {
@@ -15,9 +15,13 @@ fn main() {
     for (i, line) in splitted.iter().enumerate() {
         let numbers = get_numbers(line);
         let filtered = filter_numbers(numbers, &splitted, i);
+
+        for num in &filtered {
+            total += num;
+        }
     }
 
-    // println!("total: {:?}", total);
+    println!("total: {:?}", total);
 }
 
 fn filter_numbers(numbers: Vec<Number>, file: &Vec<&str>, line_index: usize) -> Vec<u32> {
@@ -28,7 +32,7 @@ fn filter_numbers(numbers: Vec<Number>, file: &Vec<&str>, line_index: usize) -> 
         vec![]
     };
 
-    let next = if line_index < file.len() {
+    let next = if line_index < file.len() - 1 {
         file[line_index + 1].chars().collect()
     } else {
         vec![]
@@ -36,10 +40,12 @@ fn filter_numbers(numbers: Vec<Number>, file: &Vec<&str>, line_index: usize) -> 
 
     numbers
         .into_iter()
-        .filter(|Number { num, start, end }| {
-            let mut adjacents = vec![line.get(start - 1), line.get(end + 1)];
+        .filter_map(|Number { num, start, end }| {
+            let start_adj = start.checked_sub(1).unwrap_or(0);
+            let start_line = if start > 0 { line.get(start_adj) } else { None };
+            let mut adjacents = vec![start_line, line.get(end)];
 
-            for i in start - 1..end + 1 {
+            for i in start_adj..end + 1 {
                 adjacents.push(prev.get(i));
                 adjacents.push(next.get(i));
             }
@@ -48,8 +54,13 @@ fn filter_numbers(numbers: Vec<Number>, file: &Vec<&str>, line_index: usize) -> 
                 if adjacent.is_none() {
                     return false;
                 }
+                let adj = adjacent.unwrap();
 
-                if adjacent.unwrap().to_string() != "." {
+                if adj.is_digit(10) {
+                    return false;
+                }
+
+                if adj.to_string() != "." {
                     return true;
                 }
 
@@ -57,18 +68,11 @@ fn filter_numbers(numbers: Vec<Number>, file: &Vec<&str>, line_index: usize) -> 
             });
 
             if symbol_found.is_some() {
-                true
+                Some(num)
             } else {
-                false
+                None
             }
         })
-        .map(
-            |Number {
-                 num,
-                 start: _start,
-                 end: _end,
-             }| num,
-        )
         .collect()
 }
 
@@ -88,16 +92,25 @@ fn get_numbers(line: &str) -> Vec<Number> {
 
         let size = current.chars().count();
         if size > 0 {
-            let num: u32 = current.parse().unwrap();
-            let start = start_index.unwrap();
-            let end = start + size;
-            let number = Number { num, start, end };
-
-            numbers.push(number);
+            numbers.push(get_number_info(current, start_index, size));
             start_index = None;
             current = String::new();
         }
     }
 
+    let size = current.chars().count();
+    if size > 0 {
+        numbers.push(get_number_info(current, start_index, size));
+    }
+
     numbers
+}
+
+fn get_number_info(num: String, start_index: Option<usize>, size: usize) -> Number {
+    let num: u32 = num.parse().unwrap();
+    let start = start_index.unwrap();
+    let end = start + size;
+    let number = Number { num, start, end };
+
+    number
 }
